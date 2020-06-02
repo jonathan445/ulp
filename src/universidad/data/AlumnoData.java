740696b4df3d6b77e14c9995c3fcdc3b95bae116
Eourist <50326501+Eourist@users.noreply.github.com>
2020-06-02 16:48:56 -0300

    Mejora AlumnoData y Universidad(main)

--- src/universidad/data/AlumnoData.java
+++ src/universidad/data/AlumnoData.java
@@ -4,129 +4,76 @@ import universidad.entidades.Alumno;
 import universidad.Conexion;
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.List;
 
 public class AlumnoData {
-    public boolean altaAlumno(Alumno alumno){
+    public void altaAlumno(Alumno alumno){
         try {
-            System.out.println("Nombre: " + alumno.getNombre());
-            System.out.println("FecNac: " + alumno.getFecNac());
-            System.out.println("Activo: " + alumno.getActivo() + ((true ? 1 : 0)));
-            String sql = "INSERT INTO alumno (nombre, fecNac, activo) VALUES ('" + alumno.getNombre() + "', '" + alumno.getFecNac() + "', '" + (alumno.getActivo() ? 0 : 1) + "');";
+            String sql = "INSERT INTO alumno (nombre, fecNac, activo) VALUES ('" + alumno.getNombre() + "', '" + alumno.getFecNac() + "', " + alumno.getActivo() + ");";
             Statement s = Conexion.get().createStatement();
             ResultSet rs = s.executeQuery(sql);
             
-            if (rs.first()){
-                s.close();
-                return true;
-            }
+            
+            s.close();
         } catch (SQLException e) {
             System.out.println("Error: " + e.getMessage());
         }
-        return false;
     }
     
-    public List<Alumno> obtenerAlumnos(){
-        List<Alumno> alumnos = new ArrayList<Alumno>();
-            
-
+    public ArrayList<Alumno> obtenerAlumnos(){
+        ArrayList<Alumno> resultados = new ArrayList<Alumno>();
+        
         try {
-            String sql = "SELECT * FROM alumno;";
-            PreparedStatement ps = Conexion.get().prepareStatement(sql);
-            ResultSet resultSet = ps.executeQuery();
-            Alumno alumno;
-            while(resultSet.next()){
-                alumno = new Alumno();
-                alumno.setId(resultSet.getInt("id"));
-                alumno.setNombre(resultSet.getString("nombre"));
-                alumno.setFecNac(resultSet.getDate("fecNac").toLocalDate());
-                alumno.setActivo(resultSet.getBoolean("activo"));
-
-                alumnos.add(alumno);
-            }      
-            ps.close();
-        } catch (SQLException ex) {
-            System.out.println("Error al obtener los alumnos: " + ex.getMessage());
-        }
-        
-        
-        return alumnos;
-    }
-    
-    public void borrarAlumno(int id){
-    try {
+            String sql = "SELECT * FROM alumno";
+            Statement s = Conexion.get().createStatement();
+            ResultSet rs = s.executeQuery(sql);
             
-            String sql = "DELETE FROM alumno WHERE id =?;";
-
-            PreparedStatement ps = Conexion.get().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            ps.setInt(1, id);
-                      
-            ps.executeUpdate();
-                        
-            ps.close();
-    
-        } catch (SQLException ex) {
-            System.out.println("Error al insertar un alumno: " + ex.getMessage());
+            while (rs.next()){
+                resultados.add(new Alumno(rs.getInt("ID"), rs.getString("NOMBRE"), rs.getDate("FECNAC").toLocalDate(), rs.getBoolean("ACTIVO")));
+            }
+        } catch (SQLException e){
+            System.out.println("Error:" + e.getMessage());
         }
         
-    
+        return resultados;
     }
     
-    public void actualizarAlumno(Alumno alumno){
-    
+    public void bajaAlumno(int id){
         try {
-            
-            String sql = "UPDATE alumno SET nombre = ?, fecNac = ? , "
-                    + "activo =? WHERE id = ?;";
-
-            PreparedStatement ps = Conexion.get().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            ps.setString(1, alumno.getNombre());
-            ps.setDate(2, Date.valueOf(alumno.getFecNac()));
-            ps.setBoolean(3, alumno.getActivo());
-            ps.setInt(4, alumno.getId());
-            ps.executeUpdate();
-            
-          
-            ps.close();
-    
+            String sql = "DELETE FROM alumno WHERE id = " + id + ";";
+            Statement s = Conexion.get().createStatement();
+            s.executeQuery(sql);
+            s.close();
         } catch (SQLException ex) {
-            System.out.println("Error al insertar un alumno: " + ex.getMessage());
+            System.out.println("Error: " + ex.getMessage());
         }
+    }
     
+    public void actualizarAlumno(int id, Alumno alumno){
+        try {
+            String sql = "UPDATE alumno SET nombre = " + alumno.getNombre() + ", fecNac = " + alumno.getFecNac() + " , activo = " + alumno.getActivo() + " WHERE id = " + id + ";";
+            Statement s = Conexion.get().createStatement();
+            s.execute(sql);
+            s.close();
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
     
     public Alumno buscarAlumno(int id){
-    Alumno alumno=null;
-    try {
-            
-            String sql = "SELECT * FROM alumno WHERE id =?;";
+        Alumno alumno = null;
+        try {
+            String sql = "SELECT * FROM alumno WHERE id = " + id + ";";
 
-            PreparedStatement ps = Conexion.get().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            ps.setInt(1, id);
-           
+            Statement s = Conexion.get().createStatement();
+            ResultSet rs = s.executeQuery(sql);
             
-            ResultSet resultSet=ps.executeQuery();
-            
-            while(resultSet.next()){
-                alumno = new Alumno();
-                alumno.setId(resultSet.getInt("id"));
-                alumno.setNombre(resultSet.getString("nombre"));
-                alumno.setFecNac(resultSet.getDate("fecNac").toLocalDate());
-                alumno.setActivo(resultSet.getBoolean("activo"));
-
-                
+            while(rs.next()){
+                alumno = new Alumno(rs.getInt("ID"), rs.getString("NOMBRE"), rs.getDate("FECNAC").toLocalDate(), rs.getBoolean("ACTIVO"));
             }      
-            ps.close();
-            
-            
-            
-            
-    
-        } catch (SQLException ex) {
-            System.out.println("Error al insertar un alumno: " + ex.getMessage());
+            s.close();
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
         }
-        
         return alumno;
     }
-    
 }
