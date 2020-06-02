3f0409fc2243d154b1d1a8a1e8be2d9503a1d534
Eourist <50326501+Eourist@users.noreply.github.com>
2020-06-02 12:46:58 -0300

    prueba 3

--- src/universidad/Universidad.java
+++ src/universidad/Universidad.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package universidad;
-
+import java.sql.*;
 /**
  *
  * @author jpere
@@ -16,6 +16,19 @@ public class Universidad {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
+        final String HOST = "localhost:3306";
+        final String DB = "universidad";
+        final String URL = "jdbc:mariadb://" + HOST + "/" + DB;
+        final String USUARIO = "root";
+        final String CLAVE = "";
+
+        try{
+            Connection con = DriverManager.getConnection(URL, USUARIO, CLAVE);
+            System.out.println("¡Conexión exitosa!");
+        } catch (SQLException e){
+            System.out.println("Error: " + e.getMessage());
+        }
     }
     
 }
