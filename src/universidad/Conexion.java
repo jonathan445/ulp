8be690d0df2501494b79f04d53c53865a7ad4622
Eourist <50326501+Eourist@users.noreply.github.com>
2020-06-02 17:01:14 -0300

    Fix

--- src/universidad/Conexion.java
+++ src/universidad/Conexion.java
@@ -1,48 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package universidad;
-
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
-
+import java.sql.*;
 
 public class Conexion {
+    private static String url;
+    private static String usuario;
+    private static String clave;
+
+    private static Connection conexion;
     
-    private String url="jdbc:mysql://localhost/universidad";
-    private String usuario="root";
-    private String password="";
-    private Connection conexion;
-  
-    
-    public Conexion() throws ClassNotFoundException{
-            Class.forName("org.mariadb.jdbc.Driver");
+    public Conexion(){
+        this.url = "jdbc:mariadb://localhost:3306/universidad";
+        this.usuario = "root";
+        this.clave = "";
     }
     
-    
-    public Conexion(String url, String usuario, String password) throws ClassNotFoundException {
+    public Conexion(String url, String usuario, String password){
         this.url = url;
         this.usuario = usuario;
-        this.password = password;
-
-        //Cargamos las clases de mariadb que implementan JDBC
-        Class.forName("org.mariadb.jdbc.Driver");
+        this.clave = password;
     }
     
-    
-    public Connection getConexion() throws SQLException{
+    public static Connection get(){
         if(conexion == null){
-                    // Setup the connection with the DB
-            conexion = DriverManager
-                .getConnection(url + "?useLegacyDatetimeCode=false&serverTimezone=UTC"
-                        + "&user=" + usuario + "&password=" + password);
+            try{
+                conexion = DriverManager.getConnection(url, usuario, clave);
+                System.out.println("¡Conexión exitosa!");
+            } catch (SQLException e){
+                System.out.println("Error: " + e.getMessage());
+            }
         }
         return conexion;
     }
-    
 }
+
